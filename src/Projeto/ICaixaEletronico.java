5c17792e4ae9af9c7f14422ed5c635123ffd44a1
Giovane <gabriel12zlc@gmail.com>
2026-03-15 18:32:00 -0300

    Criação de contrato de interface e atualização gitignore

---
+++ src/Projeto/ICaixaEletronico.java
@@ -0,0 +1,15 @@
+package Projeto;
+
+public interface ICaixaEletronico {
+	
+    public String pegaValorTotalDisponivel();
+    
+    public String sacar(Integer valor);
+    
+    public String pegaRelatorioCedulas();
+    
+    public String reposicaoCedulas(Integer cedula, Integer quantidade);
+    
+    public String armazenaCotaMinima(Integer minimo);
+    
+}
