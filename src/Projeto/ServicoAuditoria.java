111bbb3a112d6b3e71a91750257e299393100aba
Giovane <gabriel12zlc@gmail.com>
2026-04-11 21:14:06 -0300

    Auditoria funcionando

--- src/Projeto/ServicoAuditoria.java
+++ src/Projeto/ServicoAuditoria.java
@@ -1,5 +1,13 @@
 package projeto;
 
 public class ServicoAuditoria {
+    private String historico = "== OPERAÇÕES ==\n\n";
 
+    public void registrar(String operacao) {
+        this.historico += operacao + "\n";
+    }
+
+    public String compilarExtrato() {
+        return this.historico;
+    }
 }
